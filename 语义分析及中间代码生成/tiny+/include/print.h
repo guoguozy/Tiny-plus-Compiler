ab238b4efdeb1d2f056ab9c939016341f9a7d477
guoguozy <33777216+guoguozy@users.noreply.github.com>
2020-08-17 12:25:46 +0800

    Update

---
+++ 语义分析及中间代码生成/tiny+/include/print.h
@@ -0,0 +1,20 @@
+/****************************************************/
+/* File: print.h                                     */
+/* Created by guozy on 2020/6/22.                   */  
+/* Utility functions for the TINY compiler          */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+#ifndef _PRINT_H_
+#define _PRINT_H_
+#include "global.h"
+/* Procedure printToken prints a token 
+ * and its lexeme to the listing file
+ */
+void printToken(TokenType, const char *, const int &);
+
+void printError(const int &error_code, const int &lineno, char *error_details = nullptr);
+
+void printTree(TreeNode *root);
+
+#endif
---
+++ 语义分析及中间代码生成/tiny+/include/scan.h
@@ -0,0 +1,30 @@
+/****************************************************/
+/* File: scan.h                                     */
+/* Created by guozy on 2020/6/22.                   */               
+/* The scanner interface for the TINY compiler      */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+#include "global.h"
+#include <utility>
+#include <string>
+
+/* MAXTOKENLEN is the maximum size of a token */
+#define MAXTOKENLEN 40
+
+/* tokenString array stores the lexeme of each token */
+//extern char tokenString[MAXTOKENLEN+1];
+
+/* function getToken returns the
+ * next token in source file
+ */
+/**
+ * @param lineno 该Token的行数
+ * */
+std::pair<TokenType, std::string> getToken(int& lineno);
+
+#endif
